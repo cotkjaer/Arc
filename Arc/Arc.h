de1d90be626960e2fdf774040402cf1ebd9cc738
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 15:09:37 +0100

    Initial Commit

---
+++ Arc/Arc.h
@@ -0,0 +1,19 @@
+//
+//  Arc.h
+//  Arc
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Arc.
+FOUNDATION_EXPORT double ArcVersionNumber;
+
+//! Project version string for Arc.
+FOUNDATION_EXPORT const unsigned char ArcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Arc/PublicHeader.h>
+
+
